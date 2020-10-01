552763996fc05b403eb430cd72b10c9c8a7d8d40
Tim <tsclay9@gmail.com>
2020-10-01 11:47:34 -0400

    nav bar component in main app

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
   import HeroGrid from "./components/HeroGrid.svelte";
   import Dropdown from "./components/Dropdown.svelte";
+  import NavBar from "./components/NavBar.svelte";
+
+  import { windowWidth, needModal } from "./stores.js";
+
+  export let toggleModal;
+  export let width;
 
   let testValue, anotherTest;
   console.log(testValue, "before");
@@ -12,7 +18,28 @@
     "What is your most favorite pet ever on Earth?",
   ];
   const defaultOption = options[2];
+
+  const unsubscribeWidth = windowWidth.subscribe((value) => (width = value));
+  const unsubscribeModal = needModal.subscribe((value) => {
+    toggleModal = value;
+  });
+
+  const toggleNavButtons = () => {
+    needModal.set(!toggleModal);
+  };
 </script>
+
+<svelte:body
+  on:click={(e) => {
+    if (e.target.id !== 'modal-toggler') {
+      needModal.set(false);
+    }
+  }} />
+
+<svelte:window
+  on:resize={() => {
+    windowWidth.set(window.innerWidth);
+  }} />
 
 <!-- <HeroGrid centerMid upperRight imgSrc={'../path60.png'}>
   <div slot="center-mid">
@@ -22,7 +49,9 @@
     <p>HEY THERE YO</p>
   </div>
 </HeroGrid> -->
-<Dropdown bind:selected={testValue} {options} {defaultOption}/>
+
+<NavBar {width}/>
+<Dropdown bind:selected={testValue} {options} {defaultOption} />
 <span>{testValue}</span>
 
 <Dropdown
